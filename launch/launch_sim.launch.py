ae3e3004af5befe611186fbf76081cd48e6c2dd4
ykkim-temaat <ykkim.temaat@gmail.com>
2026-03-04 17:55:13 +0900

    2026-03-04; Test gazebo control

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -37,21 +37,30 @@ def generate_launch_description():
     # Include the Gazebo launch file, provided by the ros_gz_sim package
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
-        )]), launch_arguments={
-                'gz_args': ['-r -v4 ', world, ' --render-engine ogre'],
-                'on_exit_shutdown': 'true'
-             }.items()
+            get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+            launch_arguments={'gz_args': ['-r -v4 ', world, ' --render-engine ogre'], 'on_exit_shutdown': 'true'}.items()
     )
 
      # Run the spawner node from the ros_gz_sim package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='ros_gz_sim', 
-                        executable='create',
+    spawn_entity = Node(package='ros_gz_sim', executable='create',
                         arguments=['-topic', 'robot_description',
                                    '-name', 'study-bot',
                                    '-z', '0.1'],
                         output='screen')
     
+
+    # Launch the ROS-Gazebo bridge for normal topics
+    bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
+    ros_gz_bridge = Node(
+        package="ros_gz_bridge",
+        executable="parameter_bridge",
+        arguments=[
+            '--ros-args',
+            '-p',
+            f'config_file:={bridge_params}',
+        ]
+    )
+
 
 
     # Launch them all!
@@ -59,5 +68,6 @@ def generate_launch_description():
         rsp,
         world_arg,
         gazebo,
-        spawn_entity
+        spawn_entity,
+        ros_gz_bridge,
     ])
