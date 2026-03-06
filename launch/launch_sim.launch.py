acf458b0baab0c4cc622ac5f10b446ebd55fec39
ykkim-temaat <ykkim.temaat@gmail.com>
2026-03-06 14:14:37 +0900

    2026-03-06; add my_obstacle_world.sdf

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -22,7 +22,8 @@ def generate_launch_description():
 
     rsp = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
-            get_package_share_directory(package_name), 'launch', 'rsp.launch.py')]), launch_arguments={'use_sim_time': 'true'}.items()
+            get_package_share_directory(package_name), 'launch', 'rsp.launch.py')]),
+            launch_arguments={'use_sim_time': 'true'}.items()
     )
 
     world = LaunchConfiguration('world')
@@ -49,8 +50,7 @@ def generate_launch_description():
     
 
     # Launch the ROS-Gazebo bridge for normal topics
-    bridge_params = os.path.join(
-        get_package_share_directory(package_name),'config','gz_bridge.yaml')
+    bridge_params = os.path.join(get_package_share_directory(package_name),'config','gz_bridge.yaml')
     ros_gz_bridge = Node(
         package="ros_gz_bridge",
         executable="parameter_bridge",
